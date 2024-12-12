4327201591c8a9fc45b884ec060eea944a3a095c
BogdanBBA <bogdybba@gmail.com>
2024-12-12 21:37:45 +0100

    Update Day12.cs

--- 2024/Day12.cs
+++ 2024/Day12.cs
@@ -22,23 +22,54 @@ namespace AoC2024
             public static HashSet<Coord> Get4Neighbours(this Coord coord)
                 => [coord.Move(Cardinal.North), coord.Move(Cardinal.East), coord.Move(Cardinal.South), coord.Move(Cardinal.West)];
 
-            public static Region? IsNeighbourOfAndMatchesRegion(this List<Region> regions, Coord coord, char ch, int goDeeper)
-                => regions.FirstOrDefault(region =>
+            public static Region? IsNeighbourOfAndMatchesRegion(this List<Region> regions, Coord coord, char ch)
+                => regions.FirstOrDefault(region => coord.Get4Neighbours().Select(nC => region.TryGetValue(nC, out Cell? cell) ? cell!.Character : ' ').Distinct().Contains(ch));
+
+            public static bool IsNeighbourOf(this Region a, Region b)
+            {
+                if (a.AssumedCharacter != b.AssumedCharacter)
+                    return false;
+                List<Coord> neighboursA = [.. a.Cells.Select(cell => cell.Key).SelectMany(Get4Neighbours).Distinct()];
+                List<Coord> neighboursB = [.. b.Cells.Select(cell => cell.Key).SelectMany(Get4Neighbours).Distinct()];
+                return neighboursA.Any(neighboursB.Contains);
+            }
+
+            public static List<Region> MergeNeighbouringRegionsOfSameType(this List<Region> regions)
+            {
+                List<Region> result = [];
+                while (regions.Count > 0)
                 {
-                    HashSet<Coord> neighbourCoords = coord.Get4Neighbours();
-                    for (int _ = 0; _ < goDeeper; _++)
+                    Region nextRegion = regions[0];
+                    regions.RemoveAt(0);
+                    List<int> neighbouringRegionIndexes = [.. Enumerable.Range(0, regions.Count).Where(index => regions[index].IsNeighbourOf(nextRegion))];
+                    List<Region> conglomerate = [nextRegion];
+                    foreach (int index in neighbouringRegionIndexes.OrderByDescending(i => i))
                     {
-                        HashSet<Coord> newNeighbours = [.. neighbourCoords.SelectMany(nC => nC.Get4Neighbours())];
-                        foreach (Coord newNeighbour in newNeighbours)
-                            neighbourCoords.Add(newNeighbour);
+                        conglomerate.Add(regions[index]);
+                        regions.RemoveAt(index);
                     }
-                    return neighbourCoords.Select(nC => region.TryGetValue(nC, out Cell? cell) ? cell!.Character : ' ').Distinct().Contains(ch);
-                });
+                    result.Add(new([.. conglomerate]));
+                }
+                return result;
+            }
         }
 
         public class Region
         {
-            public Dictionary<Coord, Cell> Cells { get; init; } = [];
+            public Dictionary<Coord, Cell> Cells { get; init; }
+
+            public Region()
+            {
+                Cells = [];
+            }
+
+            public Region(params Region[] regions)
+                : this()
+            {
+                foreach (Region region in regions)
+                    foreach ((Coord coord, Cell cell) in region.Cells)
+                        Cells.Add(coord, cell);
+            }
 
             public char AssumedCharacter => Cells.First().Value.Character;
 
@@ -78,20 +109,17 @@ namespace AoC2024
         private static List<Region> DetermineRegions(NavigableMap map)
         {
             List<Region> result = [];
-            while (result.Sum(region => region.Count) < map.Cells.Count)
+            foreach ((Coord coord, Cell cell) in map.Cells)
             {
-                foreach ((Coord coord, Cell cell) in map.Cells)
+                Region? region = result.IsNeighbourOfAndMatchesRegion(coord, cell.Character);
+                if (region is null)
                 {
-                    Region? region = result.IsNeighbourOfAndMatchesRegion(coord, cell.Character, 5);
-                    if (region is null)
-                    {
-                        region = new();
-                        result.Add(region);
-                    }
-                    region.Cells[coord] = cell;
+                    region = new();
+                    result.Add(region);
                 }
+                region.Cells[coord] = cell;
             }
-            return result;
+            return result.MergeNeighbouringRegionsOfSameType();
         }
 
         private static void CalculateFencePricingForAllRegions(NavigableMap map)
